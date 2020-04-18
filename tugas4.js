5f3fc902d45a7fb96e030ca99e7aa7c210693fe3
SongKro <songkro48@gmail.com>
2020-04-19 02:15:42 +0700

    test

---
+++ tugas4.js
@@ -0,0 +1,11 @@
+function processSentence(){
+    return processSentence(`Nama saya ${name}, umur saya ${age} tahun, alamat saya di ${address}, dan saya punya hobby ${hobby}`)
+}
+
+var name = "Agus";
+var age = 30;
+var address = "Jln. Malioboro, Yogjakarta";
+var hobby = "gaming";
+
+var fullSentence = processSentence(name,age,address,hobby);
+console.log(fullSentence); // Menampilkan "Nama saya Agus, umur saya 30 tahun, alamat saya di Jln. Malioboro, Yogjakarta, dan saya 
